2f36bcf501d8ae6092d898c35aaaefe4bab5ffe2
kikoDevv <kiko.devv@gmail.com>
2025-06-14 17:58:27 +0200

    Added smoth transition effeckt to dropdown menu

--- src/components/buttons/dropDownMenu/dropDown.tsx
+++ src/components/buttons/dropDownMenu/dropDown.tsx
@@ -13,63 +13,73 @@ export default function DropDown() {
   return (
     <div className="grid relative">
       <button
-        className="bg-neutral-700 text-white font-medium rounded-full px-3 py-1"
+        className="bg-neutral-700 text-white font-medium rounded-full px-3 py-1 cursor-pointer hover:bg-neutral-600"
         onClick={() => setDropDownOpen(!isDropDownOpen)}>
-        {wordLength === 0 ? <div className="flex items-center gap-1"> <p>Lengh</p> <PiShuffle/></div> : `Length: ${wordLength}`}
+        {wordLength === 0 ? (
+          <div className="flex items-center gap-1">
+            {" "}
+            <p>Lengh</p> <PiShuffle />
+          </div>
+        ) : (
+          `Length: ${wordLength}`
+        )}
       </button>
-      {isDropDownOpen && (
-        <ul className="grid bg-blue-500 absolute top-full mt-1 rounded-md shadow-lg z-10 min-w-full">
+      <div
+        className={`absolute top-full mt-1 rounded-xl shadow-lg z-10 min-w-full bg-neutral-900 px-1 overflow-hidden transition-all duration-300 ease-out ${
+          isDropDownOpen ? "opacity-100 scale-100 translate-y-0 max-h-96" : "opacity-0 scale-95 -translate-y-2 max-h-0"
+        }`}>
+        <ul className={`grid py-2 transition-all duration-200 ${isDropDownOpen ? "translate-y-0" : "-translate-y-4"}`}>
           <li>
             <button
-              className="cursor-pointer bg-yellow-400 w-full rounded-md p-2 hover:bg-yellow-300 transition-colors"
+              className="cursor-pointer hover:bg-neutral-600 w-full rounded-md py-2 bg-neutral-700 transition-colors"
               onClick={() => handleWordLengthSelect(0)}>
               <PiShuffle />
             </button>
           </li>
           <li>
             <button
-              className="cursor-pointer bg-yellow-400 w-full rounded-md p-2 hover:bg-yellow-300 transition-colors"
+              className="cursor-pointer hover:bg-neutral-600 w-full rounded-md py-1 bg-neutral-700 transition-colors"
               onClick={() => handleWordLengthSelect(1)}>
               1
             </button>
           </li>
           <li>
             <button
-              className="cursor-pointer bg-yellow-400 w-full rounded-md p-2 hover:bg-yellow-300 transition-colors"
+              className="cursor-pointer hover:bg-neutral-600 w-full rounded-md py-1 bg-neutral-700 transition-colors"
               onClick={() => handleWordLengthSelect(2)}>
               2
             </button>
           </li>
           <li>
             <button
-              className="cursor-pointer bg-yellow-400 w-full rounded-md p-2 hover:bg-yellow-300 transition-colors"
+              className="cursor-pointer hover:bg-neutral-600 w-full rounded-md py-1 bg-neutral-700 transition-colors"
               onClick={() => handleWordLengthSelect(3)}>
               3
             </button>
           </li>
           <li>
             <button
-              className="cursor-pointer bg-yellow-400 w-full rounded-md p-2 hover:bg-yellow-300 transition-colors"
+              className="cursor-pointer hover:bg-neutral-600 w-full rounded-md py-1 bg-neutral-700 transition-colors"
               onClick={() => handleWordLengthSelect(4)}>
               4
             </button>
           </li>
           <li>
             <button
-              className="cursor-pointer bg-yellow-400 w-full rounded-md p-2 hover:bg-yellow-300 transition-colors"
+              className="cursor-pointer hover:bg-neutral-600 w-full rounded-md py-1 bg-neutral-700 transition-colors"
               onClick={() => handleWordLengthSelect(5)}>
               5
             </button>
           </li>
           <li>
             <button
-              className="cursor-pointer bg-yellow-400 w-full rounded-md p-2 hover:bg-yellow-300 transition-colors"
+              className="cursor-pointer hover:bg-neutral-600 w-full rounded-md py-1 bg-neutral-700 transition-colors"
               onClick={() => handleWordLengthSelect(6)}>
               6
             </button>
           </li>
         </ul>
-      )}
+      </div>
     </div>
   );
 }
