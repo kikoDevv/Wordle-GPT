f9a41b3f1308f2b3ab1f696fc8013d8084c9d72c
kikoDevv <kiko.devv@gmail.com>
2025-06-29 00:10:10 +0200

    Set up example .env

--- src/lib/mongodb/connection.ts
+++ src/lib/mongodb/connection.ts
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
-const connectMongoDB = async () =>{
+
+if (!MONGODB_URI) {
+  throw new Error("If you runing the project in local host you need to define the MONGODB_URI environment variable inside .env");
+}
+
+const connectMongoDB = async () => {
   try {
-   await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(MONGODB_URI);
     console.log("Connected to mongoDB");
   } catch (error) {
-    console.log("shit, ", error);
+    console.log("Connection error: ", error);
   }
 };
 
